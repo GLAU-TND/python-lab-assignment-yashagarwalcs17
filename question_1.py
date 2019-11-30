e049867295e35dbcc7c23d13eb257cd8d31719a1
yashagarwalcs17 <46733509+yashagarwalcs17@users.noreply.github.com>
2019-11-30 15:54:25 +0530

    Create question_1.py

---
+++ question_1.py
@@ -0,0 +1,15 @@
+class MyException(Exception):
+    def __init__(self, v):
+        self.v = v
+    def __str__(self):
+        return str(self.v)
+
+def xyz(a,b):
+    c=a+b
+    if c>150:
+        raise MyException('Custom Exception Occurred')
+    else:
+        return c
+
+v = xyz(24,34)
+print(v)
